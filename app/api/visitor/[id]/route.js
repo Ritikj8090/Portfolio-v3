d3d046d4be77621bec443e188dcdba7dffa1e688
Ritikj8090 <ritikj713@gmail.com>
2023-12-01 21:17:20 +0530

    s

--- app/api/visitor/[id]/route.js
+++ app/api/visitor/[id]/route.js
@@ -6,6 +6,7 @@ import Cors from 'cors';
 const cors = Cors({
     allowedMethods: ['PUT', 'OPTIONS'], // Adjust the allowed methods as needed
   });
+  
 const PUT = async (request, {params:{id}}) => {
     const {visit} = await request.json();
     await connectToDB();
