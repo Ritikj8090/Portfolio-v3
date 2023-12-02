79b21fb21d3bdc293f43967e98c4afcd8fd6c4e3
Ritikj8090 <ritikj713@gmail.com>
2023-12-02 15:16:12 +0530

    route

--- app/api/visitor/route.js
+++ app/api/visitor/route.js
@@ -6,7 +6,6 @@ import { NextResponse } from "next/server";
 export const GET = async (request) => {
   await connectToDB();
   const visit = await visitorSchema.find();
-  const res = await visitorSchema.find();
   console.log(visit)
-  return  new NextResponse(JSON.stringify({message:"ok", data:res}),{status:200})
+  return  new NextResponse(JSON.stringify({message:"ok", data:visit}),{status:200})
 }
