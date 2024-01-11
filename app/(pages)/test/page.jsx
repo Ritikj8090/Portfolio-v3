93729d41fcd6a35cde0c1b76b8b5f31cdae7c1f4
Ritikj8090 <ritikj713@gmail.com>
2024-01-11 20:00:43 +0530

    os

--- app/(pages)/test/page.jsx
+++ app/(pages)/test/page.jsx
@@ -1,14 +1,17 @@
 import Image from 'next/image'
 import React from 'react'
 import light_theme from '@/public/svg/light_theme.svg'
-
+import os from 'os';
 const page = () => {
+  const O = os.hostname()
   return (
-    <div>
-        page
-        <Image src={light_theme} alt='d'/>
+    <div className=' h-screen w-full items-center justify-center flex'>
+        <span>{O}</span>
+        
     </div>
   )
 }
 
 export default page
+
+/*<Image src={light_theme} alt='d'/> */
